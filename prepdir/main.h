b7641dc98e4d5cf847b265d2d19ee6f3f7862f0b
Otuekong <myshield02@gmail.com>
2023-04-26 13:06:54 +0100

    New files in here

---
+++ prepdir/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+void execmd(char **argv);
+
+#endif
+
