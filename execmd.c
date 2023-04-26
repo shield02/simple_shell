46d1af4b7c18832feaf83d3e626920d0bcd17cad
Otuekong <myshield02@gmail.com>
2023-04-26 12:49:19 +0100

    file for executing functions

---
+++ execmd.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * execmd - execute a command
+ * @argv: char pointer to pointer - holds an array of commands
+ * 
+ * Description: This function execute commands that are gotten
+ * from the user
+ * Return: void
+*/
+void execmd(char **argv)
+{
+	char *cmd = NULL;
+    int excmd;
+
+    if (argv)
+    {
+        cmd = argv[0];
+
+        excmd = execve(cmd, argv, NULL);
+
+        if (excmd == -1)
+        {
+            perror("./shell");
+        }
+    }
+}
