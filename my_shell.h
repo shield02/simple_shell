6bc0228a02adcddc3bbd5437479d3734ac7157ed
kazibweyassin <kazibweusama@gmail.com>
2023-04-26 01:38:38 -0700

    Protypes and functions declarations for the shell

--- my_shell.h
+++ my_shell.h
@@ -1,7 +1,9 @@
+/*Directives for the shel*/
+#define _GNU_SOURCE
 #ifndef SHELL_H
 #define SHELL_H
-#define _GNU_SOURCE
 
+/*headers required */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,11 +13,12 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+/*functions Declrations*/
 extern char **environ;
-
-int _strcmp(char *s1, char *s2);
-size_t _strncmp(char *s1, char *s2, size_t n);
 int _strlen(char *s);
+char **_get_token(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 int _putchar(char c);
@@ -24,11 +27,11 @@ char *_get_path(char **env);
 int _values_path(char **arg, char **env);
 char *_getline_command(void);
 void _getenv(char **env);
-char **_get_token(char *lineptr);
-void _exit_command(char **args, char *lineptr, int _exit);
-int _fork_fun(char **arg, char **av, char **env,
+
 char *lineptr, int np, int c);
 char *_strtok(char *str, const char *delim);
+nt _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
 
 
-#endif /* SHELL_H */
+#endif 
